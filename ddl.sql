b0a82c20dd42fbfb76faea80532ce197d6741941
Ramon Cunha <ramon.network@hotmail.com>
2023-09-29 08:01:14 -0300

    Alterado pool de conexões e realizada tunning no banco de dados

--- ddl.sql
+++ ddl.sql
@@ -1,9 +1,14 @@
 SET client_encoding = 'UTF8';
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
+SELECT pg_catalog.set_config('search_path', 'public', false); -- Defina o esquema como 'public' aqui
 SET check_function_bodies = false;
 SET xmloption = content;
 SET client_min_messages = warning;
+SET row_security = off;
 
 ALTER SCHEMA PUBLIC OWNER TO dbaccess;
 
@@ -19,12 +24,15 @@ CREATE TABLE public.pessoas (
     nascimento date not null,
     nome varchar(100) not null,
     stack varchar(255),
-    search_term varchar(255),
+    search_term text,
     primary key (id)
 );
 
 CREATE EXTENSION IF NOT EXISTS pg_trgm SCHEMA pg_catalog;
 
-CREATE INDEX idx_search_term ON public.pessoas USING gin("search_term" gin_trgm_ops);
+-- Defina o esquema 'public' novamente antes de criar o índice
+SELECT pg_catalog.set_config('search_path', 'public', false);
+
+CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_search_term ON public.pessoas USING gist("search_term" gist_trgm_ops);
 
 ALTER TABLE PUBLIC.pessoas OWNER TO dbaccess;
